a6ed1d1dc1ce62f67bc9501a0268701641e10545
heedaelee <adguy72@gmail.com>
2024-01-05 19:43:55 +0900

    feature : Model에서 data CRUD 비지니스 처리 로직 완료

--- src/app/lib/notice-db.ts
+++ src/app/lib/notice-db.ts
@@ -33,7 +33,43 @@ export const postNotice = async ({ title, body }: noticeType) => {
     const notice = new Notice({ title, body, date: Date.now() });
     await notice.save();
 
-    return { newNotice };
+    return { status: 200, notice };
+  } catch (error) {
+    return { error, status: 500 };
+  }
+};
+
+/* 삭제 */
+export const deleteNotice = async (_id: string) => {
+  try {
+    const { deletedCount } = await Notice.deleteOne({ _id });
+
+    if (deletedCount === 0) {
+      throw new Error("삭제할 데이터가 없습니다.");
+    }
+
+    return { status: 200 };
+  } catch (error) {
+    return { error, status: 500 };
+  }
+};
+
+/* 수정 */
+export const updateNotice = async (
+  _id: string,
+  title: noticeType["title"],
+  body: noticeType["body"]
+) => {
+  try {
+    const { matchedCount } = await Notice.updateOne(
+      { _id },
+      { title, body }
+    );
+
+    if (matchedCount === 0)
+      throw new Error("수정할 데이터가 없습니다.");
+
+    return { status: 200 };
   } catch (error) {
     return { error, status: 500 };
   }
